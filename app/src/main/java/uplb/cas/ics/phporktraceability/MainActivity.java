1ebcf0b52f945a8b06583efcc023be87c68c9ec9
Mark Anthony Magno <markanthonyr.magno@gmail.com>
2015-11-20 15:39:14 +0800

    init

---
+++ app/src/main/java/uplb/cas/ics/phporktraceability/MainActivity.java
@@ -0,0 +1,148 @@
+package uplb.cas.ics.phporktraceability;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import helper.SessionManager;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnTouchListener, View.OnDragListener {
+
+    private static final String LOGCAT = MainActivity.class.getSimpleName();
+    private Toolbar toolbar;
+    private ImageView iv_weaning;
+    private ImageView iv_growing;
+    private LinearLayout bot_cont;
+    private LinearLayout top_cont;
+    String function = "";
+
+    SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        session = new SessionManager(getApplicationContext());
+        session.checkLogin();
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_phpork);
+
+        iv_weaning = (ImageView) findViewById(R.id.iv_weaning);
+        iv_growing = (ImageView) findViewById(R.id.iv_growing);
+        top_cont = (LinearLayout) findViewById(R.id.top_container);
+        bot_cont = (LinearLayout) findViewById(R.id.bottom_container);
+
+        iv_weaning.setOnTouchListener(this);
+        iv_growing.setOnTouchListener(this);
+        //top_cont.setOnDragListener(this);
+        bot_cont.setOnDragListener(this);
+
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            case android.R.id.home:
+                Intent i = getIntent();
+                finish();
+                startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent e) {
+        int action = e.getAction();
+        switch(action){
+            case DragEvent.ACTION_DRAG_STARTED:
+                Log.d(LOGCAT, "Drag event started");
+                break;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                Log.d(LOGCAT, "Drag event entered into "+ v.toString());
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                Log.d(LOGCAT, "Drag event exited from " + v.toString());
+                break;
+            case DragEvent.ACTION_DROP:
+                View view = (View) e.getLocalState();
+                ViewGroup from = (ViewGroup) view.getParent();
+                from.removeView(view);
+                view.invalidate();
+                LinearLayout to = (LinearLayout) v;
+                to.addView(view);
+
+                view.setVisibility(View.VISIBLE);
+                int id = view.getId();
+                function = findViewById(id).getTag().toString();
+                int vid = to.getId();
+                if(findViewById(vid) == findViewById(R.id.bottom_container)){
+                    Toast.makeText(MainActivity.this, "Chosen " + function.toUpperCase(),
+                            Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(MainActivity.this, LocationPage.class);
+                    i.putExtra("function", function);
+                    startActivity(i);
+                }
+                Log.d(LOGCAT, "Dropped " + function);
+                break;
+            case DragEvent.ACTION_DRAG_ENDED:
+                Log.d(LOGCAT, "Drag ended");
+                break;
+            default:
+                break;
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(null, shadowBuilder, v, 0);
+            return true;
+        }
+        else { return false; }
+    }
+
+    @Override
+    public void onBackPressed(){super.onBackPressed(); finish(); }
+}
---
+++ app/src/main/java/uplb/cas/ics/phporktraceability/ViewListOfPigs.java
@@ -0,0 +1,7 @@
+package uplb.cas.ics.phporktraceability;
+
+/**
+ * Created by marmagno on 11/19/2015.
+ */
+public class ViewListOfPigs {
+}
